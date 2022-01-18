a17d5c543e37f62ccfe3d425f58ac0243b1342cf
danigarcia47 <dgarcial01@educantabria.es>
2022-01-18 12:37:57 +0100

    Actualizado

--- src/data/DatosCoches.js
+++ src/data/DatosCoches.js
@@ -1,33 +1,37 @@
-export const TitulosCoches = {
-  id: 'Matricula',
+export const TitulosTablaCoches = {
+  id: 'Matrícula',
   field1: 'Marca',
   field2: 'Modelo',
-  field3: 'Color', 
+  field3: 'Color',
 };
 
-export const DatosCoches = [
-{
-  matricula: '0000 LOL',
-  marca: 'Peugeot',
-  modelo: 'Partner',
-  color: 'Rojo',
-},
-{
-  matricula: '1111 YES',
-  marca: 'Hyundai',
-  modelo: 'IX35',
-  color: 'Verde',
-},
-{
-  matricula: '2222 WOW',
-  marca: 'Mitshubishi',
-  modelo: 'EVO 7',
-  color: 'Blanco',
-},
-{
-  matricula: '3333 Fiu',
-  marca: 'Subaru',
-  modelo: 'Impreza',
-  color: 'Azul',
-}
+// TODO: rellenar atributos que faltan para todos los coches
+export const DatosTablaCoches = [
+  {
+    matricula: '0000 LOL',
+    marca: 'Peugeot',
+    modelo: 'Partner',
+    color: 'rojo',
+  },
+  {
+    matricula: '1111 YES',
+    marca: 'Hyundai',
+    modelo: 'ix35',
+    color: 'verde',
+  },
+  {
+    matricula: '2222 WOW',
+    marca: 'Mitshubishi',
+    modelo: 'Evo 7',
+    color: 'blanco',
+    imagen:
+      'https://www.mercadoracing.org/imagenes-anuncios/26/419711/mitsubishi-evo-vii-rs.jpg',
+    descripción: 'Un coche genial',
+  },
+  {
+    matricula: '3333 FFF',
+    marca: 'Subaru',
+    modelo: 'Impreza',
+    color: 'azul',
+  },
 ];
