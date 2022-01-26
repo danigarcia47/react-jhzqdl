968ef3724b176d26efb8ca54b75a5ec39fcccd5e
danigarcia47 <dgarcial01@educantabria.es>
2022-01-26 12:57:08 +0100

    Perfil vaciado

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,12 +1,10 @@
 
 import React from 'react';
-import BootstrapTabla from './BootstrapTabla';
 
 export function Perfil() {
   return (
     <div className="main-site">
       <h1>Página de perfil</h1>
-      <BootstrapTabla/>
     </div>
   );
 }
