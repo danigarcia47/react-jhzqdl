6948fd3bf24da6b19c1a8323c94043657b08e971
danigarcia47 <dgarcial01@educantabria.es>
2022-01-29 18:33:38 +0100

    Comienzo perfil

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import Home from './Home';
 
-class Perfil extends React.Component {
-  constructor (props) {
-    super(props);
-    
-  }
-
-  render(){
+export function Perfil(){
+  if(localStorage.getItem('user') == 'Dani'){
     return (
       <div>
-      <h1></h1>
+        <h1>Mi perfil</h1>
+        <p/>
+        <h3>Nombre y apellidos:</h3>
+        <h3>Email:</h3>
+        <h3>Datos personales:</h3>
+        <h3>Avatar</h3>
       </div>
     );
-    }
+  } else {
+    return (
+      <div>
+        <h1>Datos no encontrados</h1>
+      </div>
+    );
+  }
+
 }
-export default Perfil;
